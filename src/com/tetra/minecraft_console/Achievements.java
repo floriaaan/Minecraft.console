28c61cce97daabb4a4d717f85cef3b5d0f2f1ac1
Florian Leroux <florian.leroux3@laposte.net>
2020-01-11 14:16:21 +0100

    Updated GitIgnore

---
+++ src/com/tetra/minecraft_console/Achievements.java
@@ -0,0 +1,10 @@
+package com.tetra.minecraft_console;
+
+import java.io.Serializable;
+
+public class Achievements implements Serializable {
+    public boolean[] isAchieved;
+    public String[] achievementsList;
+    public String[] achievementsDisplay = {};
+
+}
